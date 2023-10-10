1a1e2f477648eaee90e20160249629106083d44b
OmarJabri7 <omarjabri967@gmail.com>
2023-10-10 20:28:25 +0300

    added test cases user request to generate sample size randomly for all images under images folder

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-from utils import read_images, assert_inputs, generate_samples
+from utils import read_images, assert_inputs, generate_samples, random
 
 if __name__ == "__main__":
     """Extract images from data folder"""
     img_dir = input("Input images folder\n")
+    is_test = input("Generate test cases ? (Y/N)\n")
+    if is_test not in ["Y", "N"]:
+        raise AttributeError("Answer should be Y or N!")
     try:
         images, names = read_images(img_dir)
     except Exception as e:
@@ -13,16 +16,22 @@ if __name__ == "__main__":
     sample_type = input("Please input sample preference: fixed (height = width) or dynamic (height != width)\n")
     if sample_type == "fixed":
         """Get fixed sample size for each image seperately"""
-        try:
-            samples_size = [int(input("Input fixed samples size\n")) for _ in range(len(images))]
-        except:
-            raise AttributeError("Samples size should be integers!")
+        if is_test == "Y":
+            samples_size = [random.randint(0, images[i].shape[0]) for i in range(len(images))]
+        else:
+            try:
+                samples_size = [int(input("Input fixed samples size\n")) for _ in range(len(images))]
+            except:
+                raise AttributeError("Samples size should be integers!")
     elif sample_type == "dynamic":
         """Get dynamic sample size for each image seperately"""
         samples_size = []
-        for _ in range(len(images)):
-            inp = input("Input height and width as: height width \n").split(" ")
-            h, w = [int(obj) for obj in inp]
+        for i in range(len(images)):
+            if is_test == "Y":
+                h, w = random.randint(0, images[i].shape[0]), random.randint(0, images[i].shape[1])
+            else:
+                inp = input("Input height and width as: height width \n").split(" ")
+                h, w = [int(obj) for obj in inp]
             samples_size.append((h,w))
     else:
         raise AttributeError("Input sample type as either fixed or dynamic!")
